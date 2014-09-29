6820bc912fe514e41dd453e073808e59db963539
Jess Scheuring <jscheuring@gmail.com>
2014-09-29 19:33:25 -0400

    adds 3 views

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,3 +3,19 @@ var ListView = Backbone.View.extend({
     render: function() {
         $('body').append(this.el);}
 });
+
+var ListItemView = Backbone.View.extend({
+    tagName: 'li',
+    render: function() {
+        $('#fish').append(this.el);}
+});
+
+var SearchView = Backbone.View.extend({
+    tagName: 'input',
+    attributes: {type: 'search'},
+    render: function() {
+        $('body').prepend(this.el);}
+});
+
+
+$('input[type=search]').on('keyup', function() {console.log('hello');});
--- test/spec/test.js
+++ test/spec/test.js
@@ -8,16 +8,35 @@
             var list = new ListView({id: 'fish'});
             list.render();
 
-            expect($('#fish').length).to.equal(1);
+            expect($('ul#fish').length).to.equal(1);
         });
     });
+    describe('the ListItemView View', function () {
+        it('should make a li element when rendered', function() {
+            var listItem = new ListItemView({className: 'fish'});
+            listItem.render();
 
-    describe('the ListView View', function () {
-        it('should make a ul element when rendered', function() {
-            var list = new ListView({id: 'fish'});
-            list.render();
+            expect($('li.fish').length).to.equal(1);
+        });
 
-            expect($('#fish').length).to.equal(1);
+        it('should make 10 items when called over a list of 10 items', function(){
+            var tenItems = _.range(10);
+            _.each(tenItems, function(item){
+                var listItem = new ListItemView({
+                    className: 'cat',
+                    id: item});
+                listItem.render();
+            });
+
+            expect($('li.cat').length).to.equal(10);
+        });
+    });
+    describe('the SearchView View', function() {
+        it('should make search box', function() {
+            var search = new SearchView({id: 'goop'});
+            search.render();
+
+            expect($('input[type=search]#goop').length).to.equal(1);
         });
     });
 })();
