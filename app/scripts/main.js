fa36d0c0a0ab044b421ee650e7ff548443e5d40d
Jess Scheuring <jscheuring@gmail.com>
2014-09-30 01:32:03 -0400

    completes assignment

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,123 @@
+var cats = [
+    {
+        name: 'Uncle Walter Cat Scientist',
+        age: 3,
+        type: 'tabby',
+        color: 'orange'
+    }, {
+        name: 'Ink'
+    }, {
+        name: 'Leela',
+        type: 'siamese'
+    }, {
+        name: 'Kapers',
+        type: 'calico'
+    }, {
+        name: 'Smoke',
+        color: 'grey'
+    }, {
+        name: 'Stripes',
+        type: 'tabby'
+    }
+
+];
+
+var filteredCats;
+var searchTerm;
+//==============================================================================
+                                    //Views
+//==============================================================================
+
 var ListView = Backbone.View.extend({
     tagName: 'ul',
     render: function() {
-        $('body').append(this.el);}
+        $('body').append(this.el);},
+    initialize: function() {
+        this.render();
+        _.each(cats, function(cat) {
+            _.extend(cat, {className: 'cats'});
+            new ListItemView(cat);
+        });
+    }
 });
 
 var ListItemView = Backbone.View.extend({
     tagName: 'li',
     render: function() {
-        $('#fish').append(this.el);}
-});
+        this.$el.text(this.name);
+        $('#catsList').append(this.el);},
+    initialize: function(options) {
+        this.name = options.name;
+        this.render();
+        this.listenTo(this.model || $('.search'), 'change', function(){
+            // var that = this;
+            // _.each(filteredCats, function(cat){that.render(cat);});
+        });
+    }
+  });
+
 
 var SearchView = Backbone.View.extend({
     tagName: 'input',
     attributes: {type: 'search'},
     render: function() {
-        $('body').prepend(this.el);}
+        $('body').prepend(this.el);},
+    initialize: function() {
+        this.render();
+        var that = this;},
+    events: {
+        'search' : 'search'
+    },
+    search: function() {
+        searchTerm = this.$el.val();
+        filteredCats = _.filter(cats, function(cat) {
+            return cat.name === searchTerm;
+        });
+        $('.cats').remove();
+        _.each(filteredCats, function(cat) {
+            new ListItemView(cat);
+        });
+    }
 });
 
 
-$('input[type=search]').on('keyup', function() {console.log('hello');});
+
+//==============================================================================
+                                    //Models (that do nothing right now)
+//==============================================================================
+
+var ListItem = Backbone.Model.extend({
+    defaults: {
+        name: 'unnamed',
+        age: 'unknown',
+        type: 'unknown',
+        color: 'multi'
+    },
+    className: 'cat',
+    initialize: function(){
+        //render the ListItemView instance.
+    }
+});
+
+var List = Backbone.Collection.extend ({
+    defaults:{}
+});
+
+
+//==============================================================================
+                                    //Do stuff
+//==============================================================================
+
+new ListView({id: 'catsList'});
+
+new SearchView({id: 'search'});
+
+
+
+
+
+
+
+
+
+//
--- test/spec/test.js
+++ test/spec/test.js
@@ -11,6 +11,7 @@
             expect($('ul#fish').length).to.equal(1);
         });
     });
+
     describe('the ListItemView View', function () {
         it('should make a li element when rendered', function() {
             var listItem = new ListItemView({className: 'fish'});
@@ -23,20 +24,60 @@
             var tenItems = _.range(10);
             _.each(tenItems, function(item){
                 var listItem = new ListItemView({
-                    className: 'cat',
+                    className: 'giggle',
                     id: item});
                 listItem.render();
             });
 
-            expect($('li.cat').length).to.equal(10);
+            expect($('li.giggle').length).to.equal(10);
+        });
+
+        it('should show the info I feed into it', function() {
+            _.each(cats, function(cat) {
+                _.extend(cat, {className: 'kitty'});
+                var listItem = new ListItemView(cat);
+            });
+            expect($('.kitty:first').text()).to.contain('Uncle Walter Cat Scientist');
+        });
+
+        it('should have a model that feeds it data', function() {
+            //make list items w/class 'cat' using a model.
+            _.each(cats, function(cat) {
+                var listItem = new ListItem(cat);
+                console.log(listItem);
+                new ListItemView(listItem);
+            });
+
+            expect($('.cat').text()).to.contain('Kapers');
+        });
+
+        it('should re-render when its model changes', function() {
+            //make list items w/class .num and model NumModel
+            var fourItems = _.range(4);
+            _.each();
+            var listItem = new ListItemView({className: 'num'});
+
+            expect($('.num').length).to.equal(4);
+            //change the model to be 4 things
+            expect($('.num').length).to.equal(3);
+            //change the model to be 5 things
+            expect($('.num').length).to.equal(3);
         });
     });
+
     describe('the SearchView View', function() {
         it('should make search box', function() {
             var search = new SearchView({id: 'goop'});
             search.render();
 
             expect($('input[type=search]#goop').length).to.equal(1);
+        });
+
+        it('should watch for search events in itself', function() {
+            $('#goop').val('hi');
+            $('#goop').trigger('search');
+
+            expect(searchTerm).to.equal('hi');
         });
     });
 })();
